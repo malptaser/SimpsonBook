4e62a0e726e09e3de39d0248b82d6b56c0d60385
M.Alp <mtaser@burgan.com.tr>
2022-02-15 20:09:06 +0300

    View Controller düzenlendi, detailsVC eklendi..

--- Simpson Book/ViewController.swift
+++ Simpson Book/ViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// UITableViewDelegate ve UITableViewDataSource miraslarını aldırıyoruz.
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    //numberof section kaç row olacağını zorunlu olarak tanımlamamız gerekiyor. return değeri gösterir
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // nasıl bir sıra olacağı cellforrow ile
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Homer Simpson"
+        return cell
     }
 
 
